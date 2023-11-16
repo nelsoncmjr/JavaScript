3ff17b04e0f2f3c583f92c0c14cb19b37cdf1321
Nelson Melo <99028863+patamar4@users.noreply.github.com>
2023-11-16 13:10:32 -0300

    Novo exercicio Marcador de vida

--- Exercico Marcador de vida.js
+++ Exercico Marcador de vida.js
@@ -1,60 +1,19 @@
-// let vida = 100;
-// let classe, vidaTotal;
+function solucao(vida, classe) {
+    let vidaTotal = 0;
 
-// window.Number(prompt("Digite classe de inimigo de 1 a 4"))
+    if (classe === 1) {
+        vidaTotal = vida - 20;
+    } else if (classe === 2) {
+        vidaTotal = vida - 30;
+    } else if (classe === 3) {
+        vidaTotal = vida - 40;
+    } else {
+        vidaTotal = vida - 50;
+    }
 
-// if (classe === 1) {
-//     vidaTotal = vida - 20;
-//     if (vidaTotal <= 0) {
-//         console.log("Game Over");
-//     } else {
-//         console.log("Vida restante " + vidaTotal)
-//     }
-// }
-
-// if (classe === 2) {
-//     vidaTotal = vida - 30;
-//     if (vidaTotal <= 0) {
-//         console.log("Game Over");
-//     } else {
-//         console.log("Vida restante " + vidaTotal)
-//     }
-// }
-
-// if (classe === 3) {
-//     vidaTotal = vida - 40;
-//     if (vidaTotal <= 0) {
-//         console.log("Game Over");
-//     } else {
-//         console.log("Vida restante " + vidaTotal)
-//     }
-// }
-
-// if (classe === 4) {
-//     vidaTotal = vida - 50;
-//     if (vidaTotal <= 0) {
-//         console.log("Game Over");
-//     } else {
-//         console.log("Vida restante " + vidaTotal)
-//     }
-// }
-
-let vida = 100;
-let classe = 1;
-let vidaTotal = 0;
-
-if (classe === 1) {
-    vidaTotal = vida - 20;
-} else if (classe === 2) {
-    vidaTotal = vida - 30;
-} else if (classe === 3) {
-    vidaTotal = vida - 40;
-} else {
-    vidaTotal = vida - 50;
-}
-
-if (vidaTotal <= 0) {
-    console.log("Game Over");
-} else {
-    console.log("Vida restante " + vidaTotal);
+    if (vidaTotal <= 0) {
+        return "Game Over";
+    } else {
+        return "Vida restante " + vidaTotal;
+    }
 }
